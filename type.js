11869dd41458dadeaae5afd994e8a076bd8391c2
Anshun-raj <60781096+Anshun-raj@users.noreply.github.com>
2022-04-19 08:56:35 +0530

    Add files via upload

---
+++ type.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".type", {
+    strings: ["Developer", "Coder", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".type1", {
+    strings: ["Developer", "Coder", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
